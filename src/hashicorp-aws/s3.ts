af4a0ab12d2fd9ff281e440a6f0cfca94fef0656
Caleb Courier <caleb.courier@gmail.com>
2023-01-23 14:39:31 -0600

    add tags to s3

--- src/hashicorp-aws/s3.ts
+++ src/hashicorp-aws/s3.ts
@@ -5,8 +5,14 @@ import { getTfEntry, resolveValue } from './helpers';
 function parseS3Bucket (diff: TfDiff, tfPlan: Json, _tfFiles: TxtFile[], tfJson: JsonFile[]): Json {
   const tfEntry = getTfEntry(diff, tfJson);
   const name = resolveValue(diff, tfPlan, tfEntry, 'bucket');
+  const tags = resolveValue(diff, tfPlan, tfEntry, 'tags');
+  const tagSet = Object.entries(tags || {}).map(([key, value]) => ({
+    Key: key,
+    Value: value
+  }));
   return {
-    Name: name
+    Name: name,
+    TagSet: tagSet
   };
 }
 
